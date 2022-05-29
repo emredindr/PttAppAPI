b235d3943111725870ce62247e3ec425ec3c6d9f
emredindr <emredindr@gmail.com>
2022-05-30 00:55:57 +0300

    IProductService soyut sınıfı olusturuldu ve persistence katmanında concrete nesnesi oluşturuldu.

---
+++ PttAppAPI.Application/Abstractions/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PttAppAPI.Domain.Entity;
+
+namespace PttAppAPI.Application.Abstractions;
+
+public interface IProductService
+{
+    List<Product> GetAllProducts();
+}
