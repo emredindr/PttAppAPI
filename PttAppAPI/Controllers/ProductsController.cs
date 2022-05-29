a82a4d7bf624e2e29948c0e841469f7427be5a24
emredindr <emredindr@gmail.com>
2022-05-30 00:59:38 +0300

    Presentation Katmanında Product Controller Oluşturuldu.

---
+++ PttAppAPI/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PttAppAPI.Application.Abstractions;
+
+namespace PttAppAPI.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly IProductService _productService;
+    public ProductsController(IProductService productService)
+    {
+        _productService = productService;
+    }
+    [HttpGet]
+    public IActionResult GetAllProducts()
+    {
+        var product = _productService.GetAllProducts();
+
+        return Ok(product);
+    }
+}
