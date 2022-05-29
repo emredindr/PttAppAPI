3b2006acc7533a4d30b25b3a23c2c9be18a3f1ee
emredindr <emredindr@gmail.com>
2022-05-30 00:30:03 +0300

    Product modeli oluşturuldu.

---
+++ PttAppAPI.Domain/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PttAppAPI.Domain.Entity.Common;
+
+namespace PttAppAPI.Domain.Entity;
+public class Product : BaseEntity
+{
+    public int CategoryId { get; set; }
+    public int SubCategoryId { get; set; }
+    public int SubOfSubCategoryId { get; set; }
+    public string? ProductIconSource { get; set; }
+
+    public string? Title { get; set; }
+    public float Price { get; set; }
+    public string PriceWithTL => (Price.ToString() + ".00 TL");
+}
