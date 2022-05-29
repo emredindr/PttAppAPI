a7699587a85042f0307dcfc439974ad5c9413951
emredindr <emredindr@gmail.com>
2022-05-30 02:04:17 +0300

    Sql Server başlantısı için configuration yapıldı.

--- PttAppAPI.Persistence/ServiceRegistration.cs
+++ PttAppAPI.Persistence/ServiceRegistration.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using PttAppAPI.Application.Abstractions;
 using PttAppAPI.Persistence.Concretes;
+using static PttAppAPI.Persistence.Concretes.PttAppAPIDbContext;
 
 namespace PttAppAPI.Persistence;
 public static class ServiceRegistration
@@ -13,5 +15,6 @@ public static class ServiceRegistration
     public static void AddPersistenceServices(this IServiceCollection services)
     {
         services.AddSingleton<IProductService, ProductService>();
+        services.AddDbContext<PttAPIDbContext>(options => options.UseSqlServer("server=.;database=PttAppDatabase;trusted_connection=true;"));
     }
 }
