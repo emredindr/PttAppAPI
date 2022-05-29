49738c98139c30e1e209753cdceccf06c5ed657b
emredindr <emredindr@gmail.com>
2022-05-30 01:56:24 +0300

    DbContext nesnesi oluşturuldu.

---
+++ PttAppAPI.Persistence/Concretes/PttAppAPIDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PttAppAPI.Domain.Entity;
+
+namespace PttAppAPI.Persistence.Concretes;
+public class PttAppAPIDbContext
+{
+    public class PttAPIDbContext : DbContext
+    {
+        public PttAPIDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
