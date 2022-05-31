62b474d3e3fe1ff7b0776fa79128a8db70772e0b
emredindr <emredindr@gmail.com>
2022-05-31 19:45:59 +0300

    Generic Repository Design Pattern yapısında repository arayüzleri oluşturuldu.(Read and Write)

---
+++ PttAppAPI.Application/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PttAppAPI.Domain.Entity.Common;
+
+namespace PttAppAPI.Application.Repositories;
+public interface IRepository<T> where T : BaseEntity
+{
+    DbSet<T> Table { get; }
+}
