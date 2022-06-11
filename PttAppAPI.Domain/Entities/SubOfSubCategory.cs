21ed7c2dbb20762f406f2d9366ad84fac8d427f8
emredindr <emredindr@gmail.com>
2022-06-11 15:38:13 +0300

    SubOfSubCategory modeli oluşturuldu.

---
+++ PttAppAPI.Domain/Entities/SubOfSubCategory.cs
@@ -0,0 +1,10 @@
+﻿using PttAppAPI.Domain.Entity.Common;
+
+namespace PttApp.Domain.Entities;
+
+public class SubOfSubCategory : BaseEntity
+{
+    public int CategoryId { get; set; }
+    public int SubCategoryId { get; set; }
+    public string? Title { get; set; }
+}
