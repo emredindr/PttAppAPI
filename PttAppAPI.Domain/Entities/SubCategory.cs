459557efd65836d92dc7b790e771b08759640c22
emredindr <emredindr@gmail.com>
2022-06-11 15:34:05 +0300

    SubCategory modeli oluşturuldu.

---
+++ PttAppAPI.Domain/Entities/SubCategory.cs
@@ -0,0 +1,12 @@
+﻿using PttAppAPI.Domain.Entity.Common;
+
+namespace PttApp.Domain.Entities;
+
+public class SubCategory : BaseEntity
+{
+    public int CategoryId { get; set; }
+    public string? Title { get; set; }
+    public bool IsPopularForElecktronic { get; set; }
+    public bool IsPopularForBaby { get; set; }
+    public string? ImageSource { get; set; }
+}
