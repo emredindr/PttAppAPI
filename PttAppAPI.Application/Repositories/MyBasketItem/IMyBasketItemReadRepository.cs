e1e5d925b30685dd36ffb53a46e07168c4b41923
emredindr <emredindr@gmail.com>
2022-06-11 16:58:59 +0300

    MyBasketItem için read-write repository interface'leri ouşturuldu.

---
+++ PttAppAPI.Application/Repositories/MyBasketItem/IMyBasketItemReadRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PttApp.Domain.Entities;
+
+namespace PttAppAPI.Application.Repositories;
+public interface IMyBasketItemReadRepository : IReadRepository<MyBasketItem>
+{
+}
--- PttAppAPI.Persistence/Migrations/20220611124730_mig_6_CreatingAccountPageItemLoginedTable.cs
+++ PttAppAPI.Persistence/Migrations/20220611124730_mig_6_CreatingAccountPageItemLoginedTable.cs
@@ -2,31 +2,30 @@
 
 #nullable disable
 
-namespace PttAppAPI.Persistence.Migrations
-{
-    public partial class mig_6_CreatingAccountPageItemLoginedTable : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "AccountPageItemsLogined",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    ImageSource = table.Column<string>(type: "nvarchar(max)", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_AccountPageItemsLogined", x => x.Id);
-                });
-        }
+namespace PttAppAPI.Persistence.Migrations;
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "AccountPageItemsLogined");
-        }
+public partial class mig_6_CreatingAccountPageItemLoginedTable : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.CreateTable(
+            name: "AccountPageItemsLogined",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                ImageSource = table.Column<string>(type: "nvarchar(max)", nullable: true)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_AccountPageItemsLogined", x => x.Id);
+            });
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "AccountPageItemsLogined");
     }
 }
