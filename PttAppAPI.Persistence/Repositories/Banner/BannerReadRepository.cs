f66fb259fa05d3319bfc174f23f283c8e0a78d18
emredindr <emredindr@gmail.com>
2022-06-12 02:00:12 +0300

    Banner write and read repository concretes nesneleri  oluşturuldu.

---
+++ PttAppAPI.Persistence/Repositories/Banner/BannerReadRepository.cs
@@ -0,0 +1,11 @@
+﻿using PttApp.Domain.Entities;
+using PttAppAPI.Application.Repositories;
+using PttAppAPI.Persistence.Contexts;
+
+namespace PttAppAPI.Persistence.Repositories;
+public class BannerReadRepository : ReadRepository<Banner>, IBannerReadRepository
+{
+    public BannerReadRepository(PttAppAPIDbContext context) : base(context)
+    {
+    }
+}
---
+++ PttAppAPI.Persistence/Repositories/Banner/BannerWriteRepository.cs
@@ -0,0 +1,11 @@
+﻿using PttApp.Domain.Entities;
+using PttAppAPI.Application.Repositories;
+using PttAppAPI.Persistence.Contexts;
+
+namespace PttAppAPI.Persistence.Repositories;
+public class BannerWriteRepository : WriteRepository<Banner>, IBannerWriteRepository
+{
+    public BannerWriteRepository(PttAppAPIDbContext context) : base(context)
+    {
+    }
+}
