346db6a53bfc74861c235a053848498fb97315e7
emredindr <emredindr@gmail.com>
2022-06-12 01:55:50 +0300

    SubOfSubCategory write and read repository concretes nesneleri  oluşturuldu.

---
+++ PttAppAPI.Persistence/Repositories/SubOfSubCategory/SubOfSubCategoryReadRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PttApp.Domain.Entities;
+using PttAppAPI.Application.Repositories;
+using PttAppAPI.Persistence.Contexts;
+
+namespace PttAppAPI.Persistence.Repositories;
+public class SubOfSubCategoryReadRepository : ReadRepository<SubOfSubCategory>, ISubOfSubCategoryReadRepository
+
+{
+    public SubOfSubCategoryReadRepository(PttAppAPIDbContext context) : base(context)
+    {
+    }
+}
---
+++ PttAppAPI.Persistence/Repositories/SubOfSubCategory/SubOfSubCategoryWriteRepository.cs
@@ -0,0 +1,11 @@
+﻿using PttApp.Domain.Entities;
+using PttAppAPI.Application.Repositories;
+using PttAppAPI.Persistence.Contexts;
+
+namespace PttAppAPI.Persistence.Repositories;
+internal class SubOfSubCategoryWriteRepository : WriteRepository<SubOfSubCategory>, ISubOfSubCategoryWriteRepository
+{
+    public SubOfSubCategoryWriteRepository(PttAppAPIDbContext context) : base(context)
+    {
+    }
+}
