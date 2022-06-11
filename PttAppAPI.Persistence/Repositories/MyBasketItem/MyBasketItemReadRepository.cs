dc7fd2e5095722f99f08535e82ddd5039d5bf7a6
emredindr <emredindr@gmail.com>
2022-06-12 02:05:51 +0300

    MyBasketItem write and read repository concretes nesneleri  oluşturuldu.

---
+++ PttAppAPI.Persistence/Repositories/MyBasketItem/MyBasketItemReadRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PttApp.Domain.Entities;
+using PttAppAPI.Application.Repositories;
+using PttAppAPI.Persistence.Contexts;
+
+namespace PttAppAPI.Persistence.Repositories;
+public class MyBasketItemReadRepository : ReadRepository<MyBasketItem>, IMyBasketItemReadRepository
+{
+    public MyBasketItemReadRepository(PttAppAPIDbContext context) : base(context)
+    {
+    }
+}
---
+++ PttAppAPI.Persistence/Repositories/MyBasketItem/MyBasketItemWriteRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PttApp.Domain.Entities;
+using PttAppAPI.Application.Repositories;
+using PttAppAPI.Persistence.Contexts;
+
+namespace PttAppAPI.Persistence.Repositories;
+public class MyBasketItemWriteRepository : WriteRepository<MyBasketItem>,IMyBasketItemWriteRepository
+{
+    public MyBasketItemWriteRepository(PttAppAPIDbContext context) : base(context)
+    {
+    }
+}
