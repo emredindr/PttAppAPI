efa429c871b30fcaf3e6ab817aa7b3bdc0d8d17f
emredindr <emredindr@gmail.com>
2022-06-11 15:58:26 +0300

    MyBasketItem modeli oluşturuldu.

---
+++ PttAppAPI.Domain/Entities/MyBasketItem.cs
@@ -0,0 +1,15 @@
+﻿using PttAppAPI.Domain.Entity.Common;
+
+namespace PttApp.Domain.Entities;
+
+public class MyBasketItem:BaseEntity
+{
+    
+    public int ProductId { get; set; }
+    public string? ProductTitle { get; set; }
+    public int MainCategoryId { get; set; }
+    public int SubCategoryId { get; set; }
+    public int SubOfSubCategoryId { get; set; }
+    public int ProductCount { get; set; }
+    public float ProductPrice { get; set; }
+}
