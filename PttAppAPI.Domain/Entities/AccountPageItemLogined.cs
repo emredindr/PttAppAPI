b5b107c3b35cbb413a5138f683c66d33f7d4fdee
emredindr <emredindr@gmail.com>
2022-06-11 15:46:32 +0300

    AccountPageItemLogined modeli olusturuldu.

---
+++ PttAppAPI.Domain/Entities/AccountPageItemLogined.cs
@@ -0,0 +1,9 @@
+﻿using PttAppAPI.Domain.Entity.Common;
+
+namespace PttApp.Domain.Entities;
+
+public class AccountPageItemLogined : BaseEntity
+{
+    public string? Title { get; set; }
+    public string? ImageSource { get; set; }
+}
