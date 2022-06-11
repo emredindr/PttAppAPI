15c7cac411b5fb752c73a4919a9c17f7980d712c
emredindr <emredindr@gmail.com>
2022-06-11 15:11:12 +0300

    MainCategory modeli oluşturuldu.

---
+++ PttAppAPI.Domain/Entities/MainCategory.cs
@@ -0,0 +1,9 @@
+﻿using PttAppAPI.Domain.Entity.Common;
+
+namespace PttApp.Domain.Entities;
+
+public class MainCategory : BaseEntity
+{
+    public string? Title { get; set; }
+    public string? IconSource { get; set; }
+}
