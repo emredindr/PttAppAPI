ba27ec6e6fe5a939dcf41c0dc9c5d11ebb400f31
emredindr <emredindr@gmail.com>
2022-06-12 10:57:56 +0300

    IAccountService arayüzü ve concretes nesnesi oluşturuldu.

---
+++ PttAppAPI.Application/Abstractions/IAccountService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PttApp.Domain.Entities;
+
+namespace PttApp.Application.Abstractions
+{
+    public interface IAccountService
+    {
+        List<AccountPageItem> GetAccountPageItems();
+        List<AccountPageItemLogined> GetAccountPageItemsLogined();
+    }
+}
---
+++ PttAppAPI.Persistence/Concretes/AccountService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PttApp.Application.Abstractions;
+using PttApp.Domain.Entities;
+
+namespace PttAppAPI.Persistence.Concretes;
+public class AccountService : IAccountService
+{
+    public List<AccountPageItem> GetAccountPageItems()
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<AccountPageItemLogined> GetAccountPageItemsLogined()
+    {
+        throw new NotImplementedException();
+    }
+}
